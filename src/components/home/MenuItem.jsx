a9ed8dcdeee30e3027164e2b3b4b9a4b752d9165
rezky <rezky@sgo.co.id>
2021-11-21 21:09:26 +0700

    add home page

---
+++ src/components/home/MenuItem.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+class MenuItem extends React.Component {
+  render() {
+    return <div className="cr-home-menu-item"></div>;
+  }
+}
